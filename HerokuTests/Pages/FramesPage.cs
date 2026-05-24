185e7be7d9de67ad36f92264d2d3964feff65c25
Yulia <yulemona7@gmail.com>
2026-05-24 14:01:09 +0300

    Add New Page

---
+++ HerokuTests/Pages/FramesPage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuTests.Pages
+{
+    public class FramesPage
+    {
+    }
+}
