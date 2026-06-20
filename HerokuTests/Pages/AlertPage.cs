241714fe23bdac2b3b9d9690916ba1379e9fa32b
Yulia <yulemona7@gmail.com>
2026-06-20 15:49:22 +0300

    Add New Page and Test

---
+++ HerokuTests/Pages/AlertPage.cs
@@ -0,0 +1,29 @@
+﻿using HerokuTests.SeleniumFramework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuTests.Pages
+{
+    public class AlertPage:BasePage
+    {
+        private By JSAlertButtonLocator = By.XPath("//button[text()='Click for JS Alert']");
+
+        ButtonElement JSAlertButton => new ButtonElement(JSAlertButtonLocator);
+
+
+        public void ClickJSAllert()
+        {
+            IAlert alert = driver.SwitchTo().Alert();
+            alert.Accept();
+        }
+
+        public void ClickJSAlertButton()
+        {
+            JSAlertButton.ClickElement();
+        }
+    }
+}
---
+++ HerokuTests/Tests/AlertTest.cs
@@ -0,0 +1,25 @@
+﻿using HerokuTests.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuTests.Tests
+{
+    public class AlertTest:BaseTest
+    {
+        AlertPage alertPage = new AlertPage();
+        WelcomePage welcomePage = new WelcomePage();
+
+        [Test]
+        public void SimpleAlertTest()
+        {
+            welcomePage.ClickPageButton("JavaScript Alerts");
+            alertPage.ClickJSAlertButton();
+            Thread.Sleep(3000);
+            alertPage.ClickJSAllert();
+            Thread.Sleep(3000);
+        }
+    }
+}
