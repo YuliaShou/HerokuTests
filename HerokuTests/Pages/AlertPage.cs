1f32d84d7649cf7e20a189016ade166bc1cf4102
Yulia <yulemona7@gmail.com>
2026-06-28 15:31:38 +0300

    Add new methods for Alert

--- HerokuTests/Pages/AlertPage.cs
+++ HerokuTests/Pages/AlertPage.cs
@@ -11,11 +11,16 @@ namespace HerokuTests.Pages
     public class AlertPage:BasePage
     {
         private By JSAlertButtonLocator = By.XPath("//button[text()='Click for JS Alert']");
+        private By JSConfirmButtonLocator = By.XPath("//button[text()='Click for JS Confirm']");
+        private By ResultLabelLocator = By.Id("result");
+        private By JSPromptButtonLocator = By.XPath("//button[text()='Click for JS Prompt']");
 
         ButtonElement JSAlertButton => new ButtonElement(JSAlertButtonLocator);
+        ButtonElement JSConfirmButton => new ButtonElement(JSConfirmButtonLocator);
+        LabelElement ResultLabel => new LabelElement(ResultLabelLocator);
+        ButtonElement JSPromptButton => new ButtonElement(JSPromptButtonLocator);
 
-
-        public void ClickJSAllert()
+        public void ClickOkOnAllert()
         {
             IAlert alert = driver.SwitchTo().Alert();
             alert.Accept();
@@ -25,5 +30,33 @@ namespace HerokuTests.Pages
         {
             JSAlertButton.ClickElement();
         }
+
+        public void ClickJSConfirmButton() 
+        { 
+            JSConfirmButton.ClickElement();
+        }
+
+        public void ClickCancelOnAlert() 
+        {
+            IAlert alert = driver.SwitchTo().Alert();
+            alert.Dismiss();
+        }
+
+        public string GetTextFromResultLabel()
+        {
+            return ResultLabel.GetText();
+        }
+
+        public void ClickJSPromptButton() 
+        {
+            JSPromptButton.ClickElement();
+        }
+
+        public void SendTextInAlert(string text)
+        {
+            IAlert alert = driver.SwitchTo().Alert();
+            alert.SendKeys(text);
+        }
+
     }
 }
--- HerokuTests/Tests/AlertTest.cs
+++ HerokuTests/Tests/AlertTest.cs
@@ -17,9 +17,20 @@ namespace HerokuTests.Tests
         {
             welcomePage.ClickPageButton("JavaScript Alerts");
             alertPage.ClickJSAlertButton();
-            Thread.Sleep(3000);
-            alertPage.ClickJSAllert();
-            Thread.Sleep(3000);
+            alertPage.ClickOkOnAllert();
+            Assert.AreEqual(alertPage.GetTextFromResultLabel(), "You successfully clicked an alert", "Texts are not equal");
+            alertPage.ClickJSConfirmButton();
+            alertPage.ClickCancelOnAlert();
+            alertPage.GetTextFromResultLabel();
+            string textFromResultLabel= alertPage.GetTextFromResultLabel();
+            Assert.AreEqual(textFromResultLabel, "You clicked: Cancel", "Texts are not equal");
+            alertPage.ClickJSConfirmButton();
+            alertPage.ClickOkOnAllert();
+            Assert.AreEqual(alertPage.GetTextFromResultLabel(), "You clicked: Ok", "Texts are not equal");
+            alertPage.ClickJSPromptButton();
+            alertPage.SendTextInAlert("Test");
+            alertPage.ClickOkOnAllert();
+            Assert.AreEqual(alertPage.GetTextFromResultLabel(), "You entered: Test", "Texts are not equal");
         }
     }
 }
